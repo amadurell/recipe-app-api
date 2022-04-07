39292ab2b1f0f64538d10e93eb68ca91fdf4c6c9
Alexandre Madurell <alexandre.madurell@beefree.io>
2022-04-07 10:48:43 +0200

    Fixed linting and autopep config

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -26,7 +26,8 @@ class ModelTests(TestCase):
 
     def test_create_new_superuser(self):
         """Test creating a new superuser"""
-        user = get_user_model().objects.create_superuser("test@test.test", "test123")
+        user = get_user_model().objects.create_superuser("test@test.test",
+                                                         "test123")
 
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
